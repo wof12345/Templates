becaff91764cf913e1f0a28263efd0f7fbb20b69
wof12345 <lsampercot@gmail.com>
2023-02-12 18:57:02 +0600

    added newton difference

---
+++ untagged/assRandom/forwardbackwarddifference.js
@@ -0,0 +1,24 @@
+let uArrayX = [1891, 1901, 1911, 1921, 1931];
+let uArrayY = [46, 66, 81, 93, 101];
+
+let xDifferenceInterval = uArrayX[1] - uArrayX[0];
+
+let uArrayDifferenceTableArray = [[...uArrayY]];
+
+let newArray = [];
+
+let number = "";
+
+// console.log(getFactorial(3));
+
+function generateDifferenceTable() {
+  for (let j = 0; j < uArrayX.length - 1; j++) {
+    for (let i = 0; i < uArrayY.length - 1; i++) {
+      uArrayY[i] = uArrayY[i + 1] - uArrayY[i];
+    }
+    uArrayY.pop();
+
+    uArrayDifferenceTableArray.push([...uArrayY]);
+  }
+  console.log(uArrayDifferenceTableArray);
+}
